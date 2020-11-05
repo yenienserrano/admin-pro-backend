76e878967b5c4b99c51ae060a727748cd6ec55fd
Ian Serrano <yenienserrano75@gmail.com>
2020-11-05 16:29:24 -0300

    Angular en produccion

--- controller/usuarios.js
+++ controller/usuarios.js
@@ -10,9 +10,7 @@ const Usuario = require('../models/usuario');
 const getUsuarios = async(req, res) => {
 
     const desde = Number(req.query.desde) || 0
-
-    console.log(desde)
-
+    
     const [ usuarios, total ] = await Promise.all([
         Usuario
             .find({}, "role nombre email google img")
